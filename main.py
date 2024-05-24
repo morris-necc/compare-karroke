662d726c2b83d448f21dcd4c06c4da249d7978e2
morris-necc <morrissskim@gmail.com>
2024-05-24 20:34:04 +0900

    Retrieve top tracks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, url_for, session, redirect
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import time
 
 app = Flask(__name__)
 
@@ -18,12 +19,25 @@ def login():
 @app.route('/authorize')
 def authorize():
     #After the user logs in, they get redirected here
-    return "authorized"
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+
+    #flask requires this request object
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session["token_info"] = token_info #saves the token info in the session
+
+    return redirect(url_for('getTracks', _external=True))
 
 @app.route('/getTracks')
 def getTracks():
-    #placeholder
-    return "Tracks"
+    try:
+        token_info = get_token()
+    except:
+        print("User not logged in")
+        return redirect(url_for("login", _external=True))
+    sp = spotipy.Spotify(auth=token_info["access_token"])
+    return sp.current_user_top_artists(limit=50, offset=0)['items']
 
 def create_spotify_oauth():
     #Creates a SpotifyOAuth object
@@ -31,8 +45,23 @@ def create_spotify_oauth():
         client_id = "77771486cf5e471fb94e32197e9035e9",
         client_secret = "0009c35f5bd248e1a4234a1f2a765b1c",
         redirect_uri = url_for("authorize", _external=True),
-        scope = "user-library-read"
+        scope = "user-top-read"
     )
+
+def get_token():
+    #Checks if access token is expired & gets a new one if it is
+    #Also checks if there IS token data, if not redirect to login
+    token_info = session.get("token_info", {})
+
+    if not token_info:
+        raise "Execption"
+    
+    now = int(time.time())
+    is_expired = token_info["expires_at"] - now < 60
+    if is_expired:
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(token_info["refresh_token"])
+    return token_info
 
 #start
 if __name__ == '__main__':  
