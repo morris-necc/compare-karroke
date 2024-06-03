3af426ba141746d5ae94e5b97423bc710de03b3e
morris-necc <morrissskim@gmail.com>
2024-06-04 01:49:45 +0900

    Revamp
    Now with caching

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from spotipy.oauth2 import SpotifyOAuth
 import time
 import random
 from string import ascii_uppercase
+from flask_session import Session
+import os
 
 # Notes
 # You can pass variables into the html code like below:
@@ -21,11 +23,24 @@ from string import ascii_uppercase
 # then run compare()
 # pass back the resulting list
 
+# what if i need /room/code
+# maybe there's only room for 1 room & since leaving the room doesn't delete the stuff that was in there, it doesn't show new users' stuff
+# in which case, updating the room page is also broken (either it can't tell when a new person has joined or it just doesn't update right)
+
+# It could be spotify's API (i give my client secret & client id)
+
+# ok it's just not updating correctly
+# new people can't see previous results
 
 # initialize
 app = Flask(__name__)
-app.secret_key = "038nsu3sYn82y"
-app.config['SESSION_COOKIE_NAME'] = "Test Cookie"
+os.environ["SPOTIPY_CLIENT_ID"] = "77771486cf5e471fb94e32197e9035e9"
+os.environ["SPOTIPY_CLIENT_SECRET"] = "0009c35f5bd248e1a4234a1f2a765b1c"
+os.environ["SPOTIPY_REDIRECT_URI"] = 'http://localhost:5000/'
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
 socketio = SocketIO(app)
 rooms = {}
 
@@ -40,172 +55,99 @@ def generate_unique_code(length):
 
     return code
 
-@app.route('/')
-@app.route('/index')
+@app.route('/',  methods=["POST", "GET"])
+@app.route('/index',  methods=["POST", "GET"])
 def index():
-    session.clear()
-    return render_template('index.html')
-
-@app.route("/connect")
-def connect():
-    #Logs you into spotify
-    #Home page for now, but change later
-    sp_oauth = create_spotify_oauth()
-    auth_url = sp_oauth.get_authorize_url()
-    return redirect(auth_url)
-
-@app.route('/create')
-def create():
-    pass
-
-@app.route('/join')
-def join():
-    pass
-
-@app.route('/authorize')
-def authorize():
-    #After the user logqs in, they get redirected here
-    sp_oauth = create_spotify_oauth()
-    session.clear()
-
-    #flask requires this request object
-    code = request.args.get('code')
-    token_info = sp_oauth.get_access_token(code)
-    session["token_info"] = token_info #saves the token info in the session
-
-    return redirect(url_for('menu', _external=True))
-
-@app.route('/menu', methods=["POST", "GET"])
-def menu():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-top-read',
+                                               cache_handler=cache_handler,
+                                               show_dialog=True)
+           
     if request.method == "POST":
+        sp = spotipy.Spotify(auth_manager=auth_manager)
         join_code = request.form.get("join_code")
         join_btn = request.form.get("join_btn", False)
-        create = request.form.get("create", False) #returns False when they're pressed
+        create = request.form.get("create", False)
 
         if join_btn != False and not join_code:
-            return render_template('menu.html', error="Please enter a room code")
-        
+            return render_template('index.html', error="Please enter a room code")
+
         room = join_code
         if create != False:
             room = generate_unique_code(4)
             print(f"code: {room}")
             rooms[room] = {"members": 0, "tracks": []}
         elif join_code not in rooms:
-            return render_template('menu.html', error="Room does not exist", join_code=join_code)
+            return render_template('index.html', error="Room does not exist", join_code=join_code)
 
-        try:
-            token_info = get_token()
-        except:
-            print("User not logged in")
-            return redirect(url_for("login", _external=True))
-        sp = spotipy.Spotify(auth=token_info["access_token"])
-
-        session["room"] = room #semi-permanent way to store data
+        session["room"] = room
         session["name"] = sp.me()["display_name"]
+
+        print(session.get("name"))
 
         return redirect(url_for("room"))
 
-    return render_template('menu.html')
+    if request.args.get("code"):
+        # Step 2. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return render_template('index.html', flag=1)
 
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        # Step 1. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
+        return render_template('index.html', auth_url=auth_url, flag=0)
+
+    # Step 3. Signed in, display data
+           
+    return render_template('index.html')
+    
 @app.route('/room')
 def room():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+    
     room = session.get("room")
     if room is None or session.get("name") is None or room not in rooms:
-        return redirect(url_for("menu"))
+        return redirect(url_for("/"))
     
-    try:
-        token_info = get_token()
-    except:
-        print("User not logged in")
-        return redirect(url_for("login", _external=True))
-    
-    sp = spotipy.Spotify(auth=token_info["access_token"])
+    sp = spotipy.Spotify(auth_manager=auth_manager)
 
-    #to be done later
     items = []
     for i in range(0, 3):
-        for item in sp.current_user_top_tracks(limit=50, offset=i)['items']:
-            # items += {"Artist": item['artists'][0]['name'], "Title": item['name'], "Cover": item['album']['images'][1]['url']}
+        for item in sp.current_user_top_tracks(limit=50, offset=i*50)['items']:
             items += [item['album']['images'][2]['url'], item['name'], item['artists'][0]['name']]
 
     session["items"] = items
 
     return render_template('room.html', code=room, members=rooms[room]["members"])
 
-@app.route('/test')
-def test():
-    try:
-        token_info = get_token()
-    except:
-        print("User not logged in")
-        return redirect(url_for("login", _external=True))
-    
-    sp = spotipy.Spotify(auth=token_info["access_token"])
-
-    return sp.current_user_top_tracks(limit=50, offset=0)['items']
-
-
-
-def create_spotify_oauth():
-    #Creates a SpotifyOAuth object
-    return SpotifyOAuth(
-        client_id = "77771486cf5e471fb94e32197e9035e9",
-        client_secret = "0009c35f5bd248e1a4234a1f2a765b1c",
-        redirect_uri = url_for("authorize", _external=True),
-        scope = "user-top-read"
-    )
-
-def get_token():
-    #Checks if access token is expired & gets a new one if it is
-    #Also checks if there IS token data, if not redirect to login
-    token_info = session.get("token_info", {})
-
-    if not token_info:
-        raise "Exception"
-    
-    now = int(time.time())
-    is_expired = token_info["expires_at"] - now < 60
-    if is_expired:
-        sp_oauth = create_spotify_oauth()
-        token_info = sp_oauth.refresh_access_token(token_info["refresh_token"])
-
-    return token_info
-
-def compare():
-
-    pass
-
 @socketio.on("connect")
-def connect(auth):
+def on_connect(auth):
     room = session.get("room")
     name = session.get("name")
     items = session.get("items")
 
-    if not room and not name:
-        return 
+    if not room or not name:
+        return
     if room not in rooms:
         leave_room(room)
-        return 
-    
+        return
+
     join_room(room)
-
-    data = {"name": name, "items":items}
+    data = {"name": name, "items": items}
     send(data, to=room)
-
     rooms[room]["members"] += 1
 
 @socketio.on("disconnect")
-def disconnect():
+def on_disconnect():
     room = session.get("room")
-    name = session.get("name")
     leave_room(room)
-
     if room in rooms:
         rooms[room]["members"] -= 1
         if rooms[room]["members"] <= 0:
             del rooms[room]
 
-
-#start
-if __name__ == '__main__':  
-    app.run()
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
