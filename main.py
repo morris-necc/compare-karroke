c894772a09b965c6745d1d77171437465ab906fe
morris-necc <morrissskim@gmail.com>
2024-06-06 17:26:59 +0900

    Better readability

--- main.py
+++ main.py
@@ -1,44 +1,21 @@
 from flask import Flask, request, url_for, session, redirect, render_template
 from flask_socketio import join_room, leave_room, send, SocketIO, emit
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-import time
 import random
 from string import ascii_uppercase
 from flask_session import Session
 import os
-import uuid
 
 # Notes
-# You can pass variables into the html code like below:
-# using render_template("example.html", variable_name=...)
-#
-# You can get the form to perform a method
-# <form method="methodName">
-
-# Make a check for every member to include in the comparison
-
-# If it doesn't work outright (please god i beg you pls work)
-# Send message to server when a new person connects
-# Room page refreshes
-# then run compare()
-# pass back the resulting list
-
-# what if i need /room/code
-# maybe there's only room for 1 room & since leaving the room doesn't delete the stuff that was in there, it doesn't show new users' stuff
-# in which case, updating the room page is also broken (either it can't tell when a new person has joined or it just doesn't update right)
-
-# It could be spotify's API (i give my client secret & client id)
-
-# ok it's just not updating correctly
-# new people can't see previous results
+# The only problem now is that it looks ugly
+# Oh, and for some reason the emit thing doesn't work
 
 # initialize
 app = Flask(__name__)
 os.environ["SPOTIPY_CLIENT_ID"] = "77771486cf5e471fb94e32197e9035e9"
 os.environ["SPOTIPY_CLIENT_SECRET"] = "0009c35f5bd248e1a4234a1f2a765b1c"
-os.environ["SPOTIPY_REDIRECT_URI"] = 'https://compare-karroke.onrender.com/'
-#os.environ["SPOTIPY_REDIRECT_URI"] = 'http://localhost:5000/'
+# os.environ["SPOTIPY_REDIRECT_URI"] = 'https://compare-karroke.onrender.com/'
+os.environ["SPOTIPY_REDIRECT_URI"] = 'http://localhost:5000/'
 app.config['SECRET_KEY'] = os.urandom(64)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
@@ -116,7 +93,7 @@ def room():
     sp = spotipy.Spotify(auth_manager=auth_manager)
 
     items = []
-    for i in range(0, 3):
+    for i in range(0, 5):
         for item in sp.current_user_top_tracks(limit=50, offset=i*50)['items']:
             # appends the album cover, track title, artist, and song id
             items += [item['album']['images'][2]['url'], item['name'], item['artists'][0]['name'], item['id']]
