b345a4ca2fe203cd405813c132b47b4fa245fddc
morris-necc <morrissskim@gmail.com>
2024-06-07 16:41:32 +0900

    More eventlet

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app.config['SESSION_FILE_DIR'] = './.flask_session/'
 Session(app)
 
 #socketio
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='eventlet')
 rooms = {}
 
 def generate_unique_code(length):
